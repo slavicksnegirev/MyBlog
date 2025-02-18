d72b1c221d52ce2fd7a524ac3f589eb3cc9cf858
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2025-02-18 14:53:55 +0300

    add: email support

--- app/models.py
+++ app/models.py
@@ -5,7 +5,9 @@ import sqlalchemy.orm as so
 from flask_login import UserMixin
 from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
+from app import db, login, app
+from time import time
+import jwt
 
 
 followers = sa.Table(
@@ -84,6 +86,20 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except Exception as e:
+            print(e)
+            return
+        return db.session.get(User, id)
 
 
 @login.user_loader
