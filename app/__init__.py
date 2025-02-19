985bbee291bcaabe548998feee6752dd36043524
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2025-02-19 14:48:14 +0300

    add: date and time

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from flask_mail import Mail
 from config import Config
 import logging
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
