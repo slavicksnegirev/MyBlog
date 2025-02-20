72fa4339316fc3d96f056aaeab2d3743aaae08d2
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2025-02-20 13:20:50 +0300

    add: ajax

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from urllib.parse import urlsplit
+from langdetect import detect, LangDetectException
 from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from flask_babel import _, get_locale
@@ -25,7 +26,12 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
