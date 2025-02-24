73801de1b5794b8c37bdeb8e7939c11127ec2b20
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2025-02-24 11:38:32 +0300

    little fix

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -13,3 +15,4 @@ class Config:
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 10
     LANGUAGES = ['en', 'ru']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
